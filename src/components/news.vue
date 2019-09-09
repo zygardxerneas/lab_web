399ba2d67bdb2bbb20215af8b52d4d35fe4d71b3
zygardxerneas <593287187@qq.com>
2019-09-09 09:19:15 +0800

    news分页功能完成

--- src/components/news.vue
+++ src/components/news.vue
@@ -2,8 +2,18 @@
     <div class="news">
         <div class="title">NEWS</div>
         <div class="news-block" :class="isMobile? 'mobile': ''">
-            <news_item v-for="item in news" :newsitem="item" :key="item.title">
+            <news_item v-for="item in currentNews" :newsitem="item" :key="item.title">
             </news_item>
+        </div>
+        <div class="page">
+            <el-pagination
+                    small
+                    layout="prev, pager, next"
+                    :page-size="pageSize"
+                    :total="5"
+                    @current-change="handleCurrentChange"
+            >
+            </el-pagination>
         </div>
     </div>
 </template>
@@ -19,10 +29,25 @@
         data() {
             return {
                 news: [],
+                currentPage: 0,
+                pageSize: 4,
+                currentNews: []
+            }
+        },
+        watch: {
+            currentPage() {
+                this.currentNews = [];
+                for (let i=this.currentPage*this.pageSize; i<(this.currentPage+1)*this.pageSize && i<this.news.length;i++)
+                    this.currentNews.push(this.news[i]);
+            }
+        },
+        methods: {
+            handleCurrentChange(val) {
+                this.currentPage = val-1;
             }
         },
         mounted() {
-            for (let i=1;i<=3;i++) {
+            for (let i=1;i<=4;i++) {
                 this.news.push({
                     title: "这是第" + i + "条新闻",
                     abstract: "这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介" +
@@ -35,11 +60,13 @@
                 })
             }
             this.news.push({
-                title: "这是第" + 4 + "条新闻",
+                title: "这是第" + 5 + "条新闻",
                 abstract: "这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介",
                 img: require("../assets/sunkai.jpg"),
                 url: "https://cn.vuejs.org/v2/guide/index.html",
-            })
+            });
+            for (let i=this.currentPage*this.pageSize; i<(this.currentPage+1)*this.pageSize && i<this.news.length;i++)
+                this.currentNews.push(this.news[i]);
         }
     }
 </script>
@@ -65,6 +92,11 @@
             justify-content: center;
             align-items: center;
             align-content: center;
+        }
+        .page{
+            margin-top: 1rem;
+            display: flex;
+            justify-content: flex-end;
         }
     }
 </style>
