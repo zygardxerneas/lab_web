f81dcf005d9a739b4efcb086e222cbf93d4379ff
Alex Wang <whyxdt@163.com>
2019-10-26 14:04:59 +0800

    add

---
+++ src/components/contact.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="contact">
+        <div class="picture_seu">
+            <img :src="imgs" style="width: 100%;height: 100%">
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "contact",
+        props: ["isMobile"],
+        data() {
+            return {
+                imgs: null
+            }
+        },
+        mounted() {
+            this.imgs=require("../assets/contact.jpg");
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    .contact{
+        padding-left: 10%;
+        padding-right: 10%;
+        .picture_seu {
+            padding: 5% 10%;
+        }
+    }
+</style>
